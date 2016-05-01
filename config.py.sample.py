331e7e11ddff2ce5b5f9a70663f2979153fed190
Arvin Rimorin <rayarvin@veb.co.nz>
2016-05-01 09:00:51 +0800

    sample'

--- config.py.sample.py
+++ config.py.sample.py
@@ -1,6 +1,5 @@
 import os
 
-
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
@@ -12,22 +11,23 @@ class Config(object):
 class DevelopmentConfig(Config):
     DEBUG = True
     PORT = 5090
-    SECRET_KEY = 'XNV1Hm5zvOrFvEST0A0fb8972v+yIrnRR27V~3*+DG4y48`H1I,o;;tm[6>PACu'
+    SECRET_KEY = '<something sekrit>'
 
     GIPHY_BOT_USERNAME = 'giphy'
     GIPHY_BOT_ICON_URL = 'giphy.png'
     GIPHY_BOT_RATING = 'pg'
     GIPHY_BOT_SCHEME = 'https'
 
+    # giphy demo api key
     GIPHY_API_KEY = os.environ.get('GIPHY_API_KEY', 'dc6zaTOxFJmzC')
 
-    MATTERMOST_GIPHY_TOKEN = ['1iih9fzu3p88pmhka4hctdtnjh', 'i5tyfc316pdfjmawbcpk3wt1or']
+    MATTERMOST_GIPHY_TOKEN = ['<outgoing webhook token>', '<slash command token>']
 
     GITHUB_BOT_USERNAME = 'github'
     GITHUB_BOT_ICON_URL = 'https://assets-cdn.github.com/images/modules/logos_page/GitHub-Mark.png'
 
     MATTERMOST_WEBHOOK_URLS = {
-        'default': ("http://actualbrain.com:8065/hooks/ej8jcebawtbwtecpoe6kqzywja", "town-square"),
+        'default': ("<incoming webhook url>", "town-square"),
     }
 
 
